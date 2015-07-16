c29bfd37ec7cfd1f2d29637054447a709543582b
Koala Yeung <koalay@gmail.com>
2015-07-16 14:42:53 +0800

    Added webpack script bundling

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,8 +2,10 @@
 // basic
 var gulp = require('gulp');
 var browserSync = require('browser-sync');
+var webpack = require('webpack');
 
 // gulp plugins
+var gutil = require("gulp-util");
 var watch = require('gulp-watch');
 var jade = require('gulp-jade');
 var sass = require('gulp-sass');
@@ -37,6 +39,9 @@ gulp.task('watch', function() {
   watch('./src/**/*.jade', function () {
     gulp.run('templates');
   });
+  watch('./src/scripts/**/*.*', function () {
+    gulp.run('webpack');
+  });
 
 });
 
@@ -61,5 +66,23 @@ gulp.task('templates', function() {
         .pipe(gulp.dest('./public/'));
 });
 
+// bundle scripts
+gulp.task("webpack", function(callback) {
+
+  var webpackCfg = require('./webpack.config');
+
+  // run webpack
+  webpack(webpackCfg, function(err, stats) {
+    if(err) throw new gutil.PluginError("webpack", err);
+    gutil.log("[webpack]", stats.toString({
+        // output options
+    }));
+    callback();
+  });
+});
+
+// build the source files
+gulp.task('build', ['styles', 'templates', 'webpack']);
+
 // define default task(s)
-gulp.task('default', ['watch', 'serve-dev']);
+gulp.task('default', ['build', 'watch', 'serve-dev']);
